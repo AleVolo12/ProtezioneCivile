6127bcfecd5355fee3b5043c9292b872624245f5
AleVolo12 <volo.ale04@gmail.com>
2023-03-23 11:25:51 +0100

    titolo

--- Client/AttivazioneVolontari/src/routes/account.jsx
+++ Client/AttivazioneVolontari/src/routes/account.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
 import NavBar from "./navbar"
+import Titolo from "./titolo"
+
 import "../css/account.css"
 
 import img_home from "../assets/home.png"
@@ -10,6 +13,9 @@ import img_group from "../assets/gruppo-colorato.png"
 function Account(){
   return(
     <div className="home">
+      <div className="titolo">
+        <Titolo nomePagina="Account"/>
+      </div>
       <div className="bottom-navbar">
         <NavBar home={img_home} attivazione={img_attivazione} account={img_group}/>
       </div>

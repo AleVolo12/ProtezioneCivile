0076d84416beb76021dd5d88461764b3361d8aba
AleVolo12 <volo.ale04@gmail.com>
2023-03-25 10:42:24 +0100

    v

--- Client/AttivazioneVolontari/src/routes/attivazione.jsx
+++ Client/AttivazioneVolontari/src/routes/attivazione.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
+import { useState } from "react";
 
 import NavBar from "./navbar"
 import Titolo from "./titolo";
@@ -12,22 +14,36 @@ import img_group from "../assets/gruppo.png"
 import img_logo from "../assets/logo.png"
 
 function Attivazione(){
+
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: "AIzaSyAeUKUcHfUggTlFd-kR_5vmUuM16Rtfmxg",
+  });
+
+  const [indirirzzo, setIndirizzo] = useState("");
+  const [coordinate, setCoordinate] = useState("");
+
   return(
+
     <div className="attivazione">
+
       <div className="titolo">
         <Titolo nomePagina={"Segnalazione"} />
       </div>
-      <div className="top-navbar">
-        <Link to="attivazione_indicazioni">Indicazioni</Link>
+
+      <div className="mappa">
       </div>
+
+      <div className="info">
+        <input className="input" type="text" name='indirizzo' placeholder='Inserisci indirizzo...'/>
+        <input className="input" type="text" name='coordinate' placeholder='Inserisci coordinate...'/>
+      </div>  
+   
       <div className="bottom-navbar">
         <NavBar home={img_home} attivazione={img_attivazione} account={img_group}/>
       </div>
     </div>
   )
   
-  
-
 }
 
 export default Attivazione
